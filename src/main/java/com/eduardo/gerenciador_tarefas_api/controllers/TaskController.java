6a4f50ed3aeac8dcc1b5aa4212067c66d16368ad
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-09-24 16:49:50 -0300

    Criando estrutura inicial para upload de tarefas
    Criando estrutura do endpoint
    Abrindo e transformando o arquivo csv

--- src/main/java/com/eduardo/gerenciador_tarefas_api/controllers/TaskController.java
+++ src/main/java/com/eduardo/gerenciador_tarefas_api/controllers/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eduardo.gerenciador_tarefas_api.models.Task;
 import com.eduardo.gerenciador_tarefas_api.models.TaskRequestDTO;
@@ -36,6 +39,13 @@ public class TaskController {
 	public ResponseEntity<Task> create(@RequestBody TaskRequestDTO taskDto) {
 		Task taskCreated = taskService.create(taskDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+	}
+
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Void> upload(@RequestParam("arquivo") MultipartFile uploadFile,
+			@RequestParam("nome") String filename) {
+		taskService.uploadData(uploadFile);
+		return ResponseEntity.ok().build();
 	}
 
 	@PutMapping("/{taskId}")
--- src/main/java/com/eduardo/gerenciador_tarefas_api/services/TaskService.java
+++ src/main/java/com/eduardo/gerenciador_tarefas_api/services/TaskService.java
@@ -1,11 +1,16 @@
 package com.eduardo.gerenciador_tarefas_api.services;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eduardo.gerenciador_tarefas_api.exceptions.ProjectNotFoundException;
 import com.eduardo.gerenciador_tarefas_api.exceptions.TaskNotFoundException;
@@ -13,6 +18,7 @@ import com.eduardo.gerenciador_tarefas_api.exceptions.UserNotFoundException;
 import com.eduardo.gerenciador_tarefas_api.models.Project;
 import com.eduardo.gerenciador_tarefas_api.models.Task;
 import com.eduardo.gerenciador_tarefas_api.models.TaskRequestDTO;
+import com.eduardo.gerenciador_tarefas_api.models.TaskUploadRequestDTO;
 import com.eduardo.gerenciador_tarefas_api.models.User;
 import com.eduardo.gerenciador_tarefas_api.repositories.ProjectRepository;
 import com.eduardo.gerenciador_tarefas_api.repositories.TaskRepository;
@@ -72,6 +78,29 @@ public class TaskService {
 		task.setUpdatedAt(LocalDateTime.now());
 
 		taskRepository.save(task);
+	}
+
+	public void uploadData(MultipartFile uploadFile) {
+		try {
+			InputStream inputStream = uploadFile.getInputStream();
+			InputStreamReader reader = new InputStreamReader(inputStream);
+			BufferedReader buffer = new BufferedReader(reader);
+			List<TaskUploadRequestDTO> data = buffer.lines().skip(1).map(line -> this.parseLine(line)).toList();
+
+			for (TaskUploadRequestDTO t : data) {
+				System.out.println(t);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private TaskUploadRequestDTO parseLine(String line) {
+		// projectName, userEmail, taskName, taskDescription, taskStatus
+		String[] parsed = line.split(";");
+		TaskUploadRequestDTO dto = new TaskUploadRequestDTO(parsed[0], parsed[1], parsed[2], parsed[3], parsed[4]);
+		return dto;
 	}
 
 	public void delete(Long taskId) {
