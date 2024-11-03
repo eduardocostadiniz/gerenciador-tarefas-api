3a6cfe25a50bff61af25ea7fc145cc3992c678b0
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-11-03 11:56:02 -0300

    feat: Adicionando endpoint que lista as tarefas por usuário

--- src/main/java/com/eduardo/gerenciador_tarefas_api/repositories/TaskRepository.java
+++ src/main/java/com/eduardo/gerenciador_tarefas_api/repositories/TaskRepository.java
@@ -1,9 +1,10 @@
 package com.eduardo.gerenciador_tarefas_api.repositories;
 
+import com.eduardo.gerenciador_tarefas_api.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.eduardo.gerenciador_tarefas_api.models.Task;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    List<Task> findByUserId(String id);
 }
